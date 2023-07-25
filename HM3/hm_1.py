4ab07df957448aa9d4c92240e340a6b4fdc1f6fe
svetashi <svetashi@none>
2023-07-26 01:11:39 +0300

    reverting

--- HM3/hm_1.py
+++
@@ -1,9 +0,0 @@
-# Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами. В результирующем списке не должно быть дубликатов. [1, 2, 3, 1, 2, 4, 5] -> [1, 2]
-
-first_list = [1, 2, 3, 1, 2, 4, 5]
-new_list = []
-
-for i in range(0, len(first_list)-1):
-    if first_list.count(first_list[i]) > 1:
-        new_list.append(first_list[i])
-print(set(new_list))
